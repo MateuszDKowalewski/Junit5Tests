ea5c165d5517c53cc9ab06c15a7e2d847ef81adf
mkowalewski <mateusz.kowalewski@nekken.pl>
2022-11-13 16:47:42 +0100

    Initial commit

---
+++ src/test/java/pl/mkowalewski/junit5test/Junit5testApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mkowalewski.junit5test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5testApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
